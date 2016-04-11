5c47677fc15f91deae3e50442efd8c88209f3a89
Edwin <edwinvrooij@gmail.com>
2016-04-11 02:42:58 +0200

    revamp again

--- cpanel/logout.php
+++
@@ -1,8 +0,0 @@
-﻿<?php
-if(isset($_SESSION['id'])) {
-	session_destroy();
-	echo "<META HTTP-EQUIV=Refresh CONTENT=\"0; URL=?e=login\">";
-} else {
-	echo "<META HTTP-EQUIV=Refresh CONTENT=\"0; URL=?e=login\">";
-}
-?>
