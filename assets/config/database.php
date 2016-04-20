fd55063b6a3e6110747b2ed6cad4e73238ed9cce
Edwin <edwinvrooij@gmail.com>
2016-04-20 15:15:50 +0200

    experimental config update

--- assets/config/database.php
+++ assets/config/database.php
@@ -2,11 +2,24 @@
 if(basename($_SERVER["PHP_SELF"]) == "database.php"){
     die("403 - Access Forbidden");
 }
+
+//Parse the config file
+$ini_array = parse_ini_file("../config.ini", true);
+
+//Put the sections in an array
+$db_config = $ini_array['db_config'];
+$second_config = $ini_array['second_config'];
+
+//Initialize the variables
+print $db_config['host'] . "<br/>";
+print $second_config['user'];
+
 //SQL Information
-$host['hostname'] = 'localhost'; // Hostname [Usually locahost]
+$host['hostname'] = 'localhost'; // Hostname [Usually localhost]
 $host['user'] = 'dbuser'; // Database Username [Usually root]
 $host['password'] = 'dl34od903jfDow'; // Database Password [Leave blank if unsure]
 $host['database'] = 'maple_maplelife'; // Database Name
+
 
 //Database Prefix
 $prefix = "bit_";
